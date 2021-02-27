d038b9d20887b48da22c1afdd6291a8f26ae68be
Raymond Tran <raymondhieutran@gmail.com>
2021-02-28 01:08:15 +1100

    Finished adding dynamic modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Role } from "./components/role-card/role";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Grid, { GridSpacing } from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid";
 import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
 import { Backdrop, Fade } from "@material-ui/core";
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
+      border: "2px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
@@ -52,8 +52,11 @@ const App: React.FC<any> = () => {
       description: "Description for Senior Developer",
     },
   ]);
+  const [selectedRole, setSelectedRole] = useState<IRole>();
 
-  const handleOpen = () => {
+  const handleOpen = (roleId: number) => {
+    let selectedRole = roles.find((role) => role.id === roleId);
+    setSelectedRole(selectedRole);
     setOpen(true);
   };
   const handleClose = () => {
@@ -63,38 +66,38 @@ const App: React.FC<any> = () => {
   return (
     <div className="App">
       <Grid container justify="center" className={classes.root} spacing={10}>
-        {roles.map((role) => (
+        {roles.map((role: IRole) => (
           <Grid item xs>
             <Role
               key={role.id}
+              roleId={role.id}
               title={role.title}
               description={role.description}
+              clicked={(key: number) => handleOpen(key)}
             />
-            <button type="button" onClick={handleOpen}>
-              react-transition-group
-            </button>
           </Grid>
         ))}
-
-<Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
-          </div>
-        </Fade>
-      </Modal>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">{selectedRole?.title}</h2>
+              <p id="transition-modal-description">
+                {selectedRole?.description}
+              </p>
+            </div>
+          </Fade>
+        </Modal>
       </Grid>
     </div>
   );
--- src/components/role-card/role.tsx
+++ src/components/role-card/role.tsx
@@ -3,24 +3,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { Button, CardActions } from "@material-ui/core";
 
 interface IProps {
+  roleId: number;
   title: string;
   description: string;
+  clicked: (key: number) => void;
 }
 
 const useStyles = makeStyles({
-  root: {
-  },
+  root: {},
   media: {
-    display: 'flex',
+    display: "flex",
     height: 300,
-    justifyContent: 'center',
-    flexDirection: 'column'
+    justifyContent: "center",
+    flexDirection: "column",
   },
 });
 
-export const Role: React.FC<IProps> = ({title, description}) => {
+export const Role: React.FC<IProps> = ({
+  roleId,
+  title,
+  description,
+  clicked,
+}) => {
   const classes = useStyles();
 
   return (
@@ -32,6 +39,11 @@ export const Role: React.FC<IProps> = ({title, description}) => {
         <Typography variant="body2" color="textSecondary" component="p">
           {description}
         </Typography>
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => clicked(roleId)}>
+            Learn More
+          </Button>
+        </CardActions>
       </CardContent>
     </Card>
   );
