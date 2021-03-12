64c7f8e6a6a002cf1be7b18002d0c79c67e3e778
Raymond Tran <raymondhieutran@gmail.com>
2021-03-12 21:53:46 +1100

    WIP Adding accordion into modal content

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,21 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
-import { Backdrop, Fade } from "@material-ui/core";
+import { Backdrop, Container, Fade } from "@material-ui/core";
+import { IRole } from "./models";
+import ModalContent from "./components/modal-content/modalContent";
 
-interface IRole {
-  id: number;
-  title: string;
-  description: string;
-}
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    paper: {
+    modalContent: {
       backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
+      border: 0
     },
     control: {
       padding: theme.spacing(2),
@@ -64,42 +62,37 @@ const App: React.FC<any> = () => {
   };
 
   return (
-    <div className="App">
-      <Grid container justify="center" className={classes.root} spacing={10}>
-        {roles.map((role: IRole) => (
-          <Grid item xs>
-            <Role
-              key={role.id}
-              roleId={role.id}
-              title={role.title}
-              description={role.description}
-              clicked={(key: number) => handleOpen(key)}
-            />
-          </Grid>
-        ))}
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className={classes.modal}
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-            <div className={classes.paper}>
-              <h2 id="transition-modal-title">{selectedRole?.title}</h2>
-              <p id="transition-modal-description">
-                {selectedRole?.description}
-              </p>
-            </div>
-          </Fade>
-        </Modal>
-      </Grid>
-    </div>
+      <Container className="App" maxWidth="lg">
+        <Grid container justify="center" className={classes.root} spacing={4}>
+          {roles.map((role: IRole) => (
+            <Grid item xs>
+              <Role
+                key={role.id}
+                roleId={role.id}
+                title={role.title}
+                description={role.description}
+                clicked={(key: number) => handleOpen(key)}
+              />
+            </Grid>
+          ))}
+          <Modal
+            // aria-labelledby="transition-modal-title"
+            // aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <ModalContent selectedRole={selectedRole!}/>
+            </Fade>
+          </Modal>
+        </Grid>
+      </Container>
   );
 };
 
--- src/components/role-card/role.tsx
+++ src/components/role-card/role.tsx
@@ -3,7 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { Button, CardActions } from "@material-ui/core";
+import {
+  Button,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+} from "@material-ui/core";
 
 interface IProps {
   roleId: number;
@@ -14,11 +19,16 @@ interface IProps {
 
 const useStyles = makeStyles({
   root: {},
-  media: {
+  content: {
+    paddingTop: 40,
     display: "flex",
-    height: 300,
-    justifyContent: "center",
+    height: 200,
     flexDirection: "column",
+    textAlign: "left"
+  },
+  media: {
+    height: 200,
+    backgroundSize: "contain",
   },
 });
 
@@ -32,19 +42,27 @@ export const Role: React.FC<IProps> = ({
 
   return (
     <Card className={classes.root}>
-      <CardContent className={classes.media}>
-        <Typography gutterBottom variant="h4" component="h4">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {description}
-        </Typography>
-        <CardActions>
-          <Button size="small" color="primary" onClick={() => clicked(roleId)}>
-            Learn More
-          </Button>
-        </CardActions>
-      </CardContent>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://cdn1.iconfinder.com/data/icons/developer-7/64/21_education_graduate_cap_code_development_developer_programmer-512.png"
+          title="Contemplative Reptile"
+        />
+        <CardContent className={classes.content}>
+          <Typography gutterBottom variant="h4" component="h4">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActions>
+        <Button size="small" color="primary" onClick={() => clicked(roleId)}>
+          Learn More
+        </Button>
+      </CardActions>
     </Card>
   );
 };
